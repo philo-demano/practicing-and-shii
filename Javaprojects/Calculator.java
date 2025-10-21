009d387648b3a52aa3dbcf89e39caf169fca6b43
philo-demano <donaldxilan@gmail.com>
2025-10-21 16:47:51 +0200

    completed code

--- Javaprojects/Calculator.java
+++ Javaprojects/Calculator.java
@@ -7,7 +7,7 @@ class Calculator extends JFrame implements ActionListener{
     static JFrame f; 
 
     //create Textfeld,
-    static JTextFeld l;
+    static JTextField l;
     
     //store operator and operands
     String s0;
@@ -15,7 +15,7 @@ class Calculator extends JFrame implements ActionListener{
     String s2;
 
     //default constructor
-    calculator(){
+    Calculator(){
         s0 = s1 = s2 = "";
     }
 
@@ -25,18 +25,19 @@ class Calculator extends JFrame implements ActionListener{
 
         try{
             // set look and feel
-            UIManager.setLookAndFeel(UIManager.getSysmbolAndFeelClassName());
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch(Exception e){
             System.err.println(e.getMessage());
         }
 
-        calculator c = new calculator();
-        l = new JTextFeld(16);
-        l.setEdiatable(false);
+        Calculator c = new Calculator();
+        l = new JTextField(16);
+        l.setEditable(false);         // darf nicht vom User geändert werden
 
         //buttons for calculator
-        Jbutton b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,badd,bsub,bdiv,bmul,beq,bcl,bp;
+        JButton b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,badd,bsub,bdiv,bmul,beq,bcl,bp;
 
+        //number buttons
         b0 = new JButton("0");
         b1 = new JButton("1");
         b2 = new JButton("2");
@@ -48,10 +49,110 @@ class Calculator extends JFrame implements ActionListener{
         b8 = new JButton("8");
         b9 = new JButton("9");
 
+        //Operator Buttons
+        bdiv = new JButton("/");
         badd = new JButton("+");
         bsub = new JButton("-");
         bmul = new JButton("*");
+        bcl = new JButton("C");
+        bp = new JButton(".");
+        beq = new JButton("=");
+        
+        JPanel p = new JPanel();
 
+        //add to actionlistener
+        bmul.addActionListener(c);
+        badd.addActionListener(c);
+        bsub.addActionListener(c);
+        bdiv.addActionListener(c);
+        b9.addActionListener(c);
+        b8.addActionListener(c);
+        b7.addActionListener(c);
+        b6.addActionListener(c);
+        b5.addActionListener(c);
+        b4.addActionListener(c);
+        b3.addActionListener(c);
+        b2.addActionListener(c);
+        b1.addActionListener(c);
+        b0.addActionListener(c);
+        bp.addActionListener(c);
+        beq.addActionListener(c);
+        bcl.addActionListener(c);
 
+        //add to panel
+        p.add(badd);
+        p.add(b1);
+        p.add(b2);
+        p.add(b3);
+        p.add(bsub);
+        p.add(b4);
+        p.add(b5);
+        p.add(b6);
+        p.add(bmul);
+        p.add(b7);
+        p.add(b8);
+        p.add(b9);
+        p.add(bdiv);
+        p.add(beq);
+        p.add(b0);
+        p.add(beq);
+        p.add(bp);
+
+        p.setBackground(Color.blue);
+
+        f.add(p);     //add panel to frame
+        f.setSize(200,220);
+        f.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        String s = e.getActionCommand();
+
+        if((s.charAt(0) == '.')||(s.charAt(0) <= '9' && s.charAt(0) >= '0')){   //for no input
+            if(!s.equals(" ")){    
+                s2 = s2 + s;
+            }else {
+                s0 = s0 + s;
+            }
+
+            l.setText(s0 + s1 + s2);
+        }else if(s.charAt(0) == 'C'){     //to clear 
+            s0 = s1 = s2 = " ";
+            l.setText(s0 + s1 + s2);
+        } else if (s.charAt(0) == '='){   // for equals
+            double te = 0; // value for answer
+
+            switch(s1){
+                case "+" -> te = Double.parseDouble(s0) + Double.parseDouble(s2);
+                case "-" -> te = Double.parseDouble(s0) - Double.parseDouble(s2);
+                case "/" -> te = Double.parseDouble(s0) / Double.parseDouble(s2);
+                case "*" -> te = Double.parseDouble(s0) * Double.parseDouble(s2);
+                    default -> System.out.println("No operator given");
+            }
+
+            l.setText(s0 + s1 + s2 + "=" + te);
+            s0 = Double.toString(te);
+            s1 = s2 = " ";
+        }else{
+            if(s1.equals("") || s2.equals("")){
+                s1 = s;
+            } else {
+                double te = 0; // value for answer
+
+                switch(s1){
+                    case "+" -> te = Double.parseDouble(s0) + Double.parseDouble(s2);
+                    case "-" -> te = Double.parseDouble(s0) - Double.parseDouble(s2);
+                    case "/" -> te = Double.parseDouble(s0) / Double.parseDouble(s2);
+                    case "*" -> te = Double.parseDouble(s0) * Double.parseDouble(s2);
+                        default -> System.out.println("No operator given");
+                }
+
+                s0 = Double.toString(te);
+                s1 = s;     //place the operator
+                s2 = " ";   //make operand blank
+            }
+
+            l.setText(s0 + s1 + s2);
+        }
     }
 }
